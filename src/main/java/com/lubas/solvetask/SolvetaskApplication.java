8f5b6f3549c77b22b02af7c2e812f25a0be885ce
Abylai Bakytzhan <abylailuba@gmail.com>
2024-04-18 00:52:03 +0500

    init project

---
+++ src/main/java/com/lubas/solvetask/SolvetaskApplication.java
@@ -0,0 +1,13 @@
+package com.lubas.solvetask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SolvetaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SolvetaskApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lubas/solvetask/SolvetaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lubas.solvetask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SolvetaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
